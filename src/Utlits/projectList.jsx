98ceab3b2d6412ae93bac5f1629904a7b983bbe7
石博文 <7782436+Ritr@users.noreply.github.com>
2024-05-22 22:01:41 +0800

    init

---
+++ src/Utlits/projectList.jsx
@@ -0,0 +1,55 @@
+import pro1 from "../assets/img/project/1.png";
+import pro2 from "../assets/img/project/2.png";
+import pro3 from "../assets/img/project/3.png";
+import pro4 from "../assets/img/project/4.png";
+import pro5 from "../assets/img/project/5.png";
+import pro6 from "../assets/img/project/6.png";
+
+
+export const projectList = [
+    {
+      id: 1,
+      image: pro1,
+      heading: "Product Design",
+      subHeading: "Design Code",
+      category:["branding_app", "website", "all"]
+    },
+    {
+      id: 2,
+      image: pro2,
+      heading: "UI/UX Design",
+      subHeading: "Design Code",
+      category:["los_app", "landing_page", "all"]
+    },
+    {
+      id: 3,
+      image: pro3,
+      heading: "UI/UX Design",
+      subHeading: "NFT Design",
+      category:["website", "branding_app", "all"],
+    },
+    {
+      id: 4,
+      image: pro4,
+      heading: "UI/UX Design",
+      subHeading: "NFT Design",
+      category:["los_app", "landing_page", "all"]
+    },
+    {
+      id: 5,
+      image: pro5,
+      heading: "Game Design",
+      subHeading: "Game Development",
+      category:["branding_app", "website", "all"]
+    },
+    {
+      id: 6,
+      image: pro6,
+      heading: "Game Design",
+      subHeading: "Game Development",
+      category:["landing_page", "website", "all"]
+    },
+  ];
+
+
+  export const imagesList = [pro1, pro2, pro3, pro4, pro5, pro6]
